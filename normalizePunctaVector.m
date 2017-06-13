1f8b045dc7cc3d1aa393b81eba30865ba482853a
Atsushi Kajita <kajita@fixstars.com>
2017-06-13 19:17:01 -0400

    fixed a bug of variable initialization

--- normalizePunctaVector.m
+++ normalizePunctaVector.m
@@ -123,7 +123,7 @@ end
 
 transcripts = zeros(size(puncta_set_normed,6),params.NUM_ROUNDS);
 transcripts_confidence = zeros(size(puncta_set_normed,6),params.NUM_ROUNDS);
-pos = zeros(size(puncta_set_normed,6),params.NUM_ROUNDS);
+pos = zeros(size(puncta_set_normed,6),3);
 puncta_ctr = 1;
 
 
