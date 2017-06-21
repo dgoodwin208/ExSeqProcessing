b278cf51c7096b182a32a2acc57cf2bebb120206
Daniel Goodwin <dgoodwin208@gmail.com>
2017-06-21 17:31:54 -0400

    Edits to makePunctaVolumes to both remove redundant spatial filterings and to store all indices used in the puncta subvolumes. Also removed old commented code blocks from normalizePunctaVector.

--- makePunctaVolumes.m
+++ makePunctaVolumes.m
@@ -8,10 +8,6 @@ load(fullfile(params.punctaSubvolumeDir,sprintf('%s_puncta_filtered.mat',params.
 %Get the list of all registred files
 files = dir(fullfile(params.registeredImagesDir,'*.tif'));
 
-%The convertMicroscope files currently takes the alphabetical order
-%Which is not the right ordering of actual experiments
-% round_correction_indices = [10,11,12,1,2,3,4,5,6,7,8,9];
-
 organized_data_files = cell(params.NUM_ROUNDS,params.NUM_CHANNELS);
 
 chan_list = {};
@@ -24,7 +20,6 @@ for file_index = 1:length(files)
     
     %Need to crop out round number and channel
     %FULLTPSsa0916dncv_round7_chan1.tif
-
     m = regexp(files(file_index).name,'.+_round(\d+)_(\w+).tif','tokens');
     round_num = str2num(m{1}{1});
     chan_name = m{1}{2};
@@ -68,14 +63,59 @@ z_total_indices = [];
 puncta_set = zeros(params.PUNCTA_SIZE,params.PUNCTA_SIZE,params.PUNCTA_SIZE, ...
     params.NUM_ROUNDS,params.NUM_CHANNELS,num_puncta);
 
+
+
+% We first filter all puncta that are within PUNCTASIZE/2 of a boundary    
+% Because the images are registered at this point and the puncta locations
+% are taken from the params.REFERENCE_ROUND_PUNCTA
 bad_puncta_indices = [];
+for puncta_idx = 1:num_puncta
+    for c_idx = params.COLOR_VEC
+        y_indices = Y(puncta_idx) - params.PUNCTA_SIZE/2 + 1: Y(puncta_idx) + params.PUNCTA_SIZE/2;
+        x_indices = X(puncta_idx) - params.PUNCTA_SIZE/2 + 1: X(puncta_idx) + params.PUNCTA_SIZE/2;
+        z_indices = Z(puncta_idx) - params.PUNCTA_SIZE/2 + 1: Z(puncta_idx) + params.PUNCTA_SIZE/2;
+        
+        if any([x_indices y_indices z_indices]<1) || ...
+           any(x_indices>data_width) || any(y_indices>data_height) || any(z_indices>data_depth)
+            bad_puncta_indices = union(bad_puncta_indices,puncta_idx);
+            continue
+        end
+        
+        %use the meshgrid command to get all the pixels
+        [y_grid,x_grid,z_grid] = meshgrid(y_indices,x_indices,z_indices);
+        
+        x_total_indices = [x_total_indices; x_grid(:)];
+        y_total_indices = [y_total_indices; y_grid(:)];
+        z_total_indices = [z_total_indices; z_grid(:)];
+        
+    end
+end
 
-x_total_indices_cell = cell(params.NUM_ROUNDS);
-y_total_indices_cell = cell(params.NUM_ROUNDS);
-z_total_indices_cell = cell(params.NUM_ROUNDS);
+%If we want to do any other spatial filtering, do it now.
+% X_MIN = 1; X_MAX = data_width;
+% Y_MIN = 1; Y_MAX = data_height;
+% Z_MIN = 1; Z_MAX = data_depth;
+
+%Remove the indices of the bad puncta that are too close to the boundaries
+good_puncta_indices = setdiff(1:num_puncta,bad_puncta_indices);
+
+%Remove all puncta from the set that are too close to the boundary of the
+%image
+puncta_set = puncta_set(:,:,:,:,:,good_puncta_indices);
+
+%Also keep track of the location of each puncta
+Y = Y(good_puncta_indices);
+X = X(good_puncta_indices);
+Z = Z(good_puncta_indices);
+
+%update our number of puncta:
+num_puncta_filtered = length(X);
+fprintf('Filtered %i/%i puncta that were too close to the boundaries\n',...
+    num_puncta-num_puncta_filtered,num_puncta);
+clearvars num_puncta; 
+
+%Define a puncta_set object that can be parallelized
 puncta_set_cell = cell(params.NUM_ROUNDS);
-bad_puncta_indices_cell = cell(params.NUM_ROUNDS);
-
 parfor exp_idx = 1:params.NUM_ROUNDS
     disp(['round=',num2str(exp_idx)])
 
@@ -87,32 +127,15 @@ parfor exp_idx = 1:params.NUM_ROUNDS
     end
     
     disp(['[',num2str(exp_idx),'] processing puncta in parallel'])
-    x_total_indices_cell{exp_idx} = [];
-    y_total_indices_cell{exp_idx} = [];
-    z_total_indices_cell{exp_idx} = [];
-    puncta_set_cell{exp_idx} = cell(params.NUM_CHANNELS,num_puncta);
-    bad_puncta_indices_cell{exp_idx} = [];
-
-
-    %For each puncta in each channel in each round, get the volume
-    %PROVIDING that the puncta is not within PUNCTASIZE/2 of a boundary
-    for puncta_idx = 1:num_puncta
+    
+    puncta_set_cell{exp_idx} = cell(params.NUM_CHANNELS,num_puncta_filtered);
+    
+    
+    for puncta_idx = 1:num_puncta_filtered
         for c_idx = params.COLOR_VEC
             y_indices = Y(puncta_idx) - params.PUNCTA_SIZE/2 + 1: Y(puncta_idx) + params.PUNCTA_SIZE/2;
             x_indices = X(puncta_idx) - params.PUNCTA_SIZE/2 + 1: X(puncta_idx) + params.PUNCTA_SIZE/2;
             z_indices = Z(puncta_idx) - params.PUNCTA_SIZE/2 + 1: Z(puncta_idx) + params.PUNCTA_SIZE/2;
-            if any([x_indices y_indices z_indices]<1)
-                bad_puncta_indices_cell{exp_idx} = union(bad_puncta_indices_cell{exp_idx},puncta_idx);
-                continue
-            end
-            if any(x_indices>data_width) || any(y_indices>data_height) || any(z_indices>data_depth)
-                bad_puncta_indices_cell{exp_idx} = union(bad_puncta_indices_cell{exp_idx},puncta_idx);
-                continue
-            end
-            
-            x_total_indices_cell{exp_idx} = [x_total_indices_cell{exp_idx}; x_indices'];
-            y_total_indices_cell{exp_idx} = [y_total_indices_cell{exp_idx}; y_indices'];
-            z_total_indices_cell{exp_idx} = [z_total_indices_cell{exp_idx}; z_indices'];
             
             puncta_set_cell{exp_idx}{c_idx,puncta_idx} = experiment_set(y_indices,x_indices,z_indices,c_idx);
         end
@@ -120,17 +143,12 @@ parfor exp_idx = 1:params.NUM_ROUNDS
 end
 
 %%
-clear data_cols, clear data
+clear data_cols
 
 disp('reducing processed puncta')
 % reduction of parfor
 for exp_idx = 1:params.NUM_ROUNDS
-    x_total_indices = [x_total_indices; x_total_indices_cell{exp_idx}];
-    y_total_indices = [y_total_indices; y_total_indices_cell{exp_idx}];
-    z_total_indices = [z_total_indices; z_total_indices_cell{exp_idx}];
-    bad_puncta_indices = union(bad_puncta_indices,bad_puncta_indices_cell{exp_idx});
-
-    for puncta_idx = 1:num_puncta
+    for puncta_idx = 1:num_puncta_filtered
         for c_idx = params.COLOR_VEC
             if ~isequal(size(puncta_set_cell{exp_idx}{c_idx,puncta_idx}), [0 0])
                 puncta_set(:,:,:,exp_idx,c_idx,puncta_idx) = puncta_set_cell{exp_idx}{c_idx,puncta_idx};
@@ -139,19 +157,8 @@ for exp_idx = 1:params.NUM_ROUNDS
     end
 end
 
-good_puncta_indices = setdiff(1:num_puncta,bad_puncta_indices);
 
-
-%Remove all puncta from the set that are too close to the boundary of the
-%image
-puncta_set = puncta_set(:,:,:,:,:,good_puncta_indices);
-
-%Also keep track of the location of each puncta
-Y = Y(good_puncta_indices);
-X = X(good_puncta_indices);
-Z = Z(good_puncta_indices);
-
-disp('saving files')
+disp('saving files from makePunctaVolumes')
 %just save puncta_set
 save(fullfile(params.punctaSubvolumeDir,sprintf('%s_puncta_rois.mat',params.FILE_BASENAME)),...
     'puncta_set','Y','X','Z','-v7.3');
@@ -159,3 +166,20 @@ save(fullfile(params.punctaSubvolumeDir,sprintf('%s_puncta_rois.mat',params.FILE
 %save all the used location values
 save(fullfile(params.punctaSubvolumeDir,sprintf('%s_pixels_used_for_puncta.mat',params.FILE_BASENAME)),...
     'x_total_indices','y_total_indices','z_total_indices','-v7.3');
+
+
+%For reporting, create an image that shows all the windows 
+figfilename = fullfile(params.reportingDir,sprintf('%s_punctasubvolumemaxproj.fig',params.FILE_BASENAME));
+fprintf('generating output file %s\n',figfilename);
+figure('Visible','off');
+mask = zeros(size(data));
+indices_punctamask = [y_total_indices x_total_indices z_total_indices];
+%We could remove redundant puncta for speed, but I actually think that it 
+%is slower than simply linearly indexing across all 3D image
+%[indices_unique, ia, ic] = unique(indices_punctamask,'rows');
+indices_linear = sub2ind(img_dim,indices_punctamask(:,1),indices_punctamask(:,2),indices_punctamask(:,3));
+mask(indices_linear)=1;
+
+imagesc(max(mask,[],3));
+title('Max Z projection of all puncta subvolumes');
+saveas(gcf,figfilename,'fig')
--- normalizePunctaVector.m
+++ normalizePunctaVector.m
@@ -1,17 +1,8 @@
+%Load the ROIS, calculate normalized intensities, then make transcript base
+%calls
+
 loadParameters;
-% load('/Users/Goody/Neuro/ExSeq/rajlab/splintr1/transcriptsv3.mat');
 load(fullfile(params.punctaSubvolumeDir,sprintf('%s_puncta_rois.mat',params.FILE_BASENAME)));
-
-%TODO: the punctaset collection contains that are too close to the boundary
-%so we filter those out in the extract_transcripts file using the
-%good_puncta_indices vector. The 'bad' indices of the puncta_set simply
-%contain zeros. So we need to first get the transcripts and puncta_set
-%vectors to agree
-% puncta_set = puncta_set(:,:,:,:,:,good_puncta_indices);
-% Y = Y(good_puncta_indices);
-% X = X(good_puncta_indices);
-% Z = Z(good_puncta_indices);
-
 
 %The puncta_set vector is of dimensions:
 %(PUNCTA_SIZE,PUNCTA_SIZE,PUNCTA_SIZE,NUM_ROUNDS,NUM_CHANNELS,num_puncta)
@@ -31,13 +22,10 @@ for exp_idx = 1:params.NUM_ROUNDS
     for c = params.COLOR_VEC
         chan_col(:,c) = reshape(puncta_set(:,:,:,exp_idx,c,:),[],1);
     end
-%     chan_col(:,2) = reshape(puncta_set(:,:,:,exp_idx,2,:),[],1);
-%     chan_col(:,3) = reshape(puncta_set(:,:,:,exp_idx,4,:),[],1);
       
     %How many unique values do we see per channel? (only useful to check
     %for unintentional 8-bit data)
-    
-    figure;
+    figure('Visible','off');
     subplot(length(params.COLOR_VEC),1,1)
     for c = params.COLOR_VEC
         subplot(length(params.COLOR_VEC),1,c)
@@ -45,22 +33,8 @@ for exp_idx = 1:params.NUM_ROUNDS
         unique_vals_count = length(unique(chan_col(:,c)));
         title(sprintf('Exp %i: Histogram of Channel %i. Unique values %i',exp_idx,c, unique_vals_count));
     end
-    
-%     subplot(3,1,2)
-%     h = histogram(chan_col(:,2),100);
-%     unique_vals_count = length(unique(chan_col(:,2)));
-%     title(sprintf('Exp %i: Histogram of Channel 2. Unique values %i',exp_idx, unique_vals_count));
-%     
-%     subplot(3,1,3)
-%     
-%     h = histogram(chan_col(:,3),100);
-%     unique_vals_count = length(unique(chan_col(:,3)));
-%     title(sprintf('Exp %i: Histogram of Channel 4. Unique values %i',exp_idx, unique_vals_count));
-   
-    %V6 was quantilenorm
-%     cols_normed = quantilenorm(chan_col);
-    %v7 is mean normed
-    %v8 is subtracting the mean before dividing by the norm
+       
+
     chan_col = chan_col - repmat(min(chan_col,[],1),size(chan_col,1),1);
     mean_vec = mean(chan_col,1);
     cols_normed = chan_col ./ repmat(mean_vec,size(chan_col,1),1);
@@ -68,64 +42,16 @@ for exp_idx = 1:params.NUM_ROUNDS
     for c = params.COLOR_VEC
         puncta_set_normed(:,:,:,exp_idx,c,:) = reshape(cols_normed(:,c),squeeze(size(puncta_set(:,:,:,exp_idx,c,:))));
     end
-%     puncta_set_normed(:,:,:,exp_idx,2,:) = reshape(cols_normed(:,2),squeeze(size(puncta_set(:,:,:,exp_idx,1,:))));
-%     puncta_set_normed(:,:,:,exp_idx,4,:) = reshape(cols_normed(:,3),squeeze(size(puncta_set(:,:,:,exp_idx,1,:))));
-exp_idx
+
+    fprintf('Normalized the puncta for sequencing round %i\n',exp_idx);
 end
-
-
-%% Visualize scores
-
-% for puncta_idx = 1:size(puncta_set_normed,6)
-%     figure(1);
-%     %     transcript_puncta = transcripts(puncta_idx,:);
-%     %     transcriptconfidence_puncta = transcripts_confidence(puncta_idx,:);
-%     
-%     subplot_idx = 1;
-%     
-%     for exp_idx = 1:NUM_ROUNDS
-%         
-%         punctaset_perround = squeeze(puncta_set_normed(:,:,:,exp_idx,:,puncta_idx));
-%         
-%         max_intensity = max(max(max(max(punctaset_perround))))+1;
-%         min_intensity = min(min(min(min(punctaset_perround))));
-%         values = zeros(4,1);
-%         
-%         [max_chan, confidence,scores] = chooseChannelMod(punctaset_perround,...
-%             params.COLOR_VEC,params.DISTANCE_FROM_CENTER);
-%         transcript_puncta = max_chan;
-%         
-%         for c_idx = 1:NUM_CHANNELS
-%             
-%             clims = [min_intensity,max_intensity];
-%             subplot(params.NUM_ROUNDS,params.NUM_CHANNELS,subplot_idx)
-%             data = squeeze(punctaset_perround(:,:,:,c_idx));
-%             imagesc(max(data,[],3),clims);
-%             
-%             if(c_idx==transcript_puncta)
-%                 title(sprintf('Conf:%d', confidence));
-%             end
-%             
-%             axis off; colormap gray
-%             subplot_idx = subplot_idx+1;
-%         end
-%     end
-%     pause
-% end
-
-
-%% And what kind of transcripts does this produce?
-% copied from the extractTranscripts.m file
 
 
 % Rows are sequencing rounds, columns are channels, press enter to go to
 % next one
-
 transcripts = zeros(size(puncta_set_normed,6),params.NUM_ROUNDS);
 transcripts_confidence = zeros(size(puncta_set_normed,6),params.NUM_ROUNDS);
 pos = zeros(size(puncta_set_normed,6),3);
-puncta_ctr = 1;
-
 
 for puncta_idx = 1:size(puncta_set_normed,6)
     
@@ -143,16 +69,13 @@ for puncta_idx = 1:size(puncta_set_normed,6)
         confidence_vector(exp_idx) = confidence;
     end
     
-    transcripts(puncta_ctr,:) = answer_vector;
-    transcripts_confidence(puncta_ctr,:) = confidence_vector;
-    pos(puncta_ctr,:) = [Y(puncta_idx),X(puncta_idx),Z(puncta_idx)];
-    puncta_ctr = puncta_ctr +1;
+    transcripts(puncta_idx,:) = answer_vector;
+    transcripts_confidence(puncta_idx,:) = confidence_vector;
+    pos(puncta_idx,:) = [Y(puncta_idx),X(puncta_idx),Z(puncta_idx)];
     
-    fprintf('Calling base puncta #%i out of %i \n',puncta_ctr, size(puncta_set_normed,6));
-    
-    
+    fprintf('Calling base puncta #%i out of %i \n',puncta_idx, size(puncta_set_normed,6));
 end
 
 save(fullfile(params.transcriptResultsDir,sprintf('%s_transcriptsv9.mat',params.FILE_BASENAME)),'transcripts','transcripts_confidence','pos');
-
+save(fullfile(params.transcriptResultsDir,sprintf('%s_puncta_normedrois.mat',params.FILE_BASENAME)),'puncta_set_normed');
 disp('Completed normalizePuncta.m and saved the transcriptsv9 mat file in the transcripts folder');
