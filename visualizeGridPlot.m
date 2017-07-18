d5101b02a336d16d15271f75092e9261b6540a7c
Daniel Goodwin <dgoodwin208@gmail.com>
2017-07-18 15:38:46 -0400

    first test of combining puncta correction with the creation of subvolumes

---
+++ visualizeGridPlot.m
@@ -0,0 +1,49 @@
+function visualizeGridPlot(puncta,transcripts,params,fignum)
+
+    
+    figure(fignum);
+    
+    clf('reset')
+    ha = tight_subplot(params.NUM_ROUNDS,params.NUM_CHANNELS,zeros(params.NUM_ROUNDS,2)+.01);
+    
+    subplot_idx = 1;
+    for exp_idx = 1:params.NUM_ROUNDS
+        
+        punctaset_perround = squeeze(puncta(:,:,:,exp_idx,:));
+        
+        max_intensity = max(max(max(max(punctaset_perround))))+1;
+        min_intensity = min(min(min(min(punctaset_perround))));
+        
+        for c_idx = 1:params.NUM_CHANNELS
+            clims = [min_intensity,max_intensity];
+            
+            %Get the subplot index using the tight_subplot system
+            axes(ha(subplot_idx));
+            
+            punctaVol = squeeze(punctaset_perround(:,:,:,c_idx));
+            
+            %imagesc(max(punctaVol,[],3),clims);
+            imagesc(max(squeeze(punctaVol(:,:,5:6)),[],3),clims);
+            %imagesc(squeeze(punctaVol(:,:,5)));
+            axis off;
+            if numel(transcripts)>1 && c_idx==transcripts(exp_idx)
+                title(sprintf('%i',c_idx),'Color','m')
+            end
+            if c_idx==1 && exp_idx ==1
+                
+                position_string = '';
+                text(-0.0,10.,position_string,'rotation',90)
+                 axis tight;
+                
+            else
+                axis off;
+            end
+            
+            colormap gray
+            subplot_idx = subplot_idx+1;
+        end
+    end
+    
+   
+
+end
