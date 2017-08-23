a16f5f6846319739c42ba1e0222d801d783c1eae
Daniel Goodwin <dgoodwin208@gmail.com>
2017-08-23 14:02:17 -0400

    fix: when making RGB images, order channels correctly

--- makeRGBImageFrom4ChanData.m
+++ makeRGBImageFrom4ChanData.m
@@ -1,13 +1,12 @@
 function [ rgbimg ] = makeRGBImageFrom4ChanData(data4d  )
 %MAKERGBIMAGEFROM4CHANDATA Take an (X,Y,C) dataset and turn it into a 3D
 %image in the RGB format. Tailored the colors to 
-% Chan 1 = Red
-% Chan 2 = Blue
-% Chan 3 = Green
-% Chan 4 = Magenta
-
-AB = imfuse(data4d(:,:,1),data4d(:,:,3),'ColorChannels',[1 0 2]);
-CD = imfuse(data4d(:,:,2),data4d(:,:,4),'ColorChannels',[2 1 2]);
+% Chan 1 = Blue
+% Chan 2 = Green
+% Chan 3 = Magenta
+% Chan 4 = Red
+AB = imfuse(data4d(:,:,1),data4d(:,:,4),'ColorChannels',[2 0 1]);
+CD = imfuse(data4d(:,:,2),data4d(:,:,3),'ColorChannels',[2 1 2]);
 rgbimg=AB+CD;
 
 end
