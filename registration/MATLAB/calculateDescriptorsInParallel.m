37a94925afcd273408251b16fcb405961e082140
Karl Marrett <karl@tuna.media.mit.edu>
2017-11-07 10:35:42 -0500

    clean descriptor parallel, still needs proper arg list in calls to calcDesc

--- registration/MATLAB/calculateDescriptorsInParallel.m
+++ registration/MATLAB/calculateDescriptorsInParallel.m
@@ -26,16 +26,10 @@ function calculateDescriptorsInParallel(run_num_list)
     while job_idx <= max_jobs || sum(running_jobs) > 0
         % check that number of jobs currently running is valid
         if (job_idx <= max_running_jobs) && (sum(running_jobs) < max_running_jobs)
-            % determine run_number idx 
-            run_num  = run_num_list(ceil(job_idx / desc_size));
+            [run_num, target_idx] = getJobIds(run_num_list, job_idx, desc_size)
 
-            % convert job_idx 1 - 9 t
-            target_idx = mod(job_idx, desc_size);
-            if target_idx == 0
-                target_idx = desc_size;
-            end
-
-            disp(['create batch (',num2str(job_idx),') run_num=',num2str(run_num),', target_idx=',num2str(target_idx)])
+            disp(['create batch (', num2str(job_idx), ') run_num=', ...
+                num2str(run_num),', target_idx=',num2str(target_idx)])
             running_jobs(job_idx) = 1; % mark as running
             jobs{job_idx} = batch(cluster, @calculateDescriptors, ... 
                 0, {run_num, target_idx, target_idx}, 'Pool', 2, 'CaptureDiary', true);
@@ -45,28 +39,19 @@ function calculateDescriptorsInParallel(run_num_list)
                 job = jobs{job_id};
                 is_finished = 0;
 
-                % determine run_number idx 
-                run_num  = run_num_list(ceil(job_idx / desc_size));
-
-                % convert job_idx 1 - 9 t
-                target_idx = mod(job_idx, desc_size);
-                if target_idx == 0
-                    target_idx = desc_size;
-                end
+                [run_num, target_idx] = getJobIds(run_num_list, job_idx, desc_size)
                 if strcmp(job.State,'finished')
                     disp(['batch (',num2str(job_id),') has ',job.State,'.'])
                     diary(job, ['./matlab-calcDesc-',num2str(run_num),'-',num2str(target_idx),'.log']);
                     running_jobs(job_id) = 0;
                     delete(job)
                     is_finished = 1;
-                elseif strcmp(job.State,'failed')
+                elseif strcmp(job.State,'failed') % Retry block
                     disp(['batch (',num2str(job_id),') has ',job.State,', resubmit it.'])
                     diary(job, ['./matlab-calcDesc-', num2str(run_num), '-', ... 
                         num2str(target_idx),'-failed.log']);
-                    % FIXME the retry to correct function
-                    jobs{job_id} = batch(cluster,@normalizeImage,0, ...
-                       {src_folder_name,dst_folder_name,fileroot_name,channels,job_id}, ...
-                       'CaptureDiary',true);
+                    jobs{job_idx} = batch(cluster, @calculateDescriptors, ... 
+                        0, {run_num, target_idx, target_idx}, 'Pool', 2, 'CaptureDiary', true);
                 end
             end
             if is_finished == 0
@@ -75,41 +60,6 @@ function calculateDescriptorsInParallel(run_num_list)
             end
         end
     end
-
-    %jobs = cell(1, run_size);
-    %for i = 1:run_size
-        %run_num  = run_num_list(ceil(i / desc_size));
-        %target_idx = mod(i, desc_size);
-        %if target_idx == 0
-            %target_idx = desc_size;
-        %end
-        %disp(['create batch (',num2str(i),') run_num=',num2str(run_num),', target_idx=',num2str(target_idx)])
-        %jobs{i} = batch(cluster,@calculateDescriptors,0,{run_num,target_idx,target_idx},'Pool',2,'CaptureDiary',true);
-    %end
-    %toc;
-
-    %tic;
-    %disp('waiting batch jobs...')
-    %for i = 1:run_size
-        %run_num    = run_num_list(ceil(i / desc_size));
-        %target_idx = mod(i, desc_size);
-        %if target_idx == 0
-            %target_idx = desc_size;
-        %end
-        %wait(jobs{i})
-        %diary(jobs{i},['./matlab-calcDesc-',num2str(run_num),'-',num2str(target_idx),'.log']);
-    %end
-
-    %disp('all batch jobs finished')
-    %toc;
-
-    %tic;
-    %disp('delete batch jobs')
-    %for i = 1:run_size
-        %delete(jobs{i})
-    %end
-    %toc;
-
 
 end
 
---
+++ registration/MATLAB/getJobIds.m
@@ -0,0 +1,14 @@
+% INPUTS:
+% run_num_list is the index list of the experiment for the specified sample
+% job_idx is the int number assigned to each job 
+% desc_size is the total number of discretized sections of the image
+function [target_idx, run_num] = getJobIds(run_num_list, job_idx, desc_size)
+    % determine run_number idx 
+    run_num  = run_num_list(ceil(job_idx / desc_size));
+
+    % convert job_idx 1 - 9
+    target_idx = mod(job_idx, desc_size);
+    if target_idx == 0
+        target_idx = desc_size;
+    end
+end
