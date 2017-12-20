36671e05dd73e662b32551187334bf1481da4033
Karl Marrett <karl@fixstars.com>
2017-12-19 22:22:13 -0500

    add funcionality for @normalizeImage, @calculateDescriptor to batch_processing.m

--- batch_process.m
+++ batch_process.m
@@ -1,4 +1,4 @@
-function batch_processing(prefix, func, args, run_num_list)
+function batch_processing(prefix, func, run_num_list, args)
 
     loadExperimentParams;
 
@@ -10,8 +10,13 @@ function batch_processing(prefix, func, args, run_num_list)
 
     tic; 
     disp('===== create batch jobs =====') 
-    max_jobs = length(run_num_list);
     max_running_jobs = params.JOB_SIZE;
+    max_jobs = length(run_num_list);
+    if func == @calculateDescriptors
+        run_num_list_size = length(run_num_list);
+        desc_size = params.ROWS_DESC * params.COLS_DESC;
+        max_jobs  = run_num_list_size * desc_size;
+    end
     waiting_sec = 10;
 
     jobs = cell(1, max_jobs);
@@ -21,7 +26,19 @@ function batch_processing(prefix, func, args, run_num_list)
     while job_idx <= max_jobs || sum(running_jobs) > 0
         % check that number of jobs currently running is valid
         if (job_idx <= max_jobs) && (sum(running_jobs) < max_running_jobs)
+
+            % determine args
             run_num = run_num_list(job_idx);
+            if func == @registerWithDescriptors
+                args = {run_num};
+            else if func == @calculateDescriptors
+                [run_num, target_idx] = getJobIds(run_num_list, job_idx, desc_size);
+                args = {run_num, target_idx, target_idx};
+            else if func == @normalizeImage
+                % FIXME add one more element to cell
+                args = {args, run_num};
+            end
+
             disp(['create batch ', num2str(job_idx)]);
             running_jobs(job_idx) = 1; % mark as running
             jobs{job_idx} = batch(cluster, func, ... 
@@ -30,18 +47,33 @@ function batch_processing(prefix, func, args, run_num_list)
         else
             for job_idx_running = find(running_jobs==1)
                 job = jobs{job_idx_running};
-                run_num = run_num_list(job_idx_running);
                 is_finished = 0;
+
+                % determine args
+                run_num = run_num_list(job_idx_running);
+                postfix = num2str(run_num);
+                if func == @registerWithDescriptors
+                    args = {run_num};
+                else if func == @calculateDescriptors
+                    [run_num, target_idx] = getJobIds(run_num_list, job_idx_running, desc_size);
+                    args = {run_num, target_idx, target_idx};
+                    postfix = [num2str(run_num), '-', num2str(target_idx)];
+                else if func == @normalizeImage
+                    % FIXME add one more element to cell
+                    args = {args, run_num};
+                end
 
                 if strcmp(job.State,'finished')
                     if isempty(getReport(job.Tasks(1).Error))
+                        % batch finished with no error
                         disp(['batch (',num2str(job_idx_running),') has ', job.State,'.']);
-                        diary(job, ['./matlab-', prefix, '-', num2str(run_num), '.log']);
+                        diary(job, ['./matlab-', prefix, '-', postfix, '.log']);
                     else
+                        %batch finished with internal error
                         disp(['batch (',num2str(job_idx_running),') had fatal internal error, see log file.']);
-                        fn = strcat('./matlab-', prefix, '-', num2str(run_num), '-fatal.log');
+                        fn = strcat('./matlab-', prefix, '-', postfix, '-fatal.log');
                         diary(job, fn);
-                        % guarantee error message gets logged
+                        % guarantee error message gets logged (appended)
                         error_msg = getReport(job.Tasks(1).Error)
                         fid = fopen(fn, 'a');
                         fprintf(fid, error_msg);
@@ -50,9 +82,10 @@ function batch_processing(prefix, func, args, run_num_list)
                     running_jobs(job_idx_running) = 0;
                     delete(job);
                     is_finished = 1;
-                elseif strcmp(job.State, 'failed') % Retry block
+                elseif strcmp(job.State, 'failed') 
+                    % batch call failed, retry
                     disp(['batch (',num2str(job_idx_running),') has ',job.State,', resubmit it.']);
-                    diary(job, ['./matlab-', prefix, '-', num2str(run_num), '-failed.log']);
+                    diary(job, ['./matlab-', prefix, '-', postfix, '-failed.log']);
                     jobs{job_idx_running} = batch(cluster, func, ... 
                         0, args, 'Pool', 2, 'CaptureDiary', true);
                 end
