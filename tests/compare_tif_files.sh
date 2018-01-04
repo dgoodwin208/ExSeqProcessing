4ede6707d8ad6e0d61f15075eb4f673bb118f187
Atsushi Kajita <kajita@fixstars.com>
2018-01-04 14:47:46 -0500

    added a script to compare tif files.

---
+++ tests/compare_tif_files.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+TEST_DIR=$1
+RIGHT_DATA_DIR=$2
+
+F1=($(\ls $TEST_DIR/*.tif))
+F2=($(\ls $RIGHT_DATA_DIR/*.tif))
+
+if [ ${#F1[*]} -ne ${#F2[*]} ]; then
+    echo "# of tiffs in TEST_DIR       = ${#F1[*]}"
+    echo "# of tiffs in RIGHT_DATA_DIR = ${#F2[*]}"
+    exit 1
+fi
+
+for((i=0; i<${#F1[*]}; i++)); do
+#    echo ${F1[$i]} ${F2[$i]}
+    ret=$(diff ${F1[$i]} ${F2[$i]})
+#    echo "ret = $ret"
+
+    if [ "$ret" != "" ]; then
+        echo "different!"
+        exit 1
+    fi
+done
+
+exit 0
+
