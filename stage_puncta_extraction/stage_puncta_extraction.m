afc6e471c7d83062db96a51d4766c6dd78d80229
Daniel Goodwin <dgoodwin208@gmail.com>
2020-11-19 13:40:03 -0500

    changing the puncta extract stage to re-do FOVs with missing rounds

--- stage_puncta_extraction/stage_puncta_extraction.m
+++ stage_puncta_extraction/stage_puncta_extraction.m
@@ -5,13 +5,25 @@ function stage_puncta_extraction()
     %later debugging
     copyfile('loadParameters.m',fullfile(params.punctaSubvolumeDir,...
         sprintf('loadParameters.m.log-%s',date)))
+    
     [ret,messages] = check_files_in_puncta_extraction();
+    
     if ret
-        fprintf('already processed puncta extraction\n');
-        fprintf('[DONE]\n');
-        return
+        %IN BRANCH: 
+        %Check the number of rounds that have been processed 
+        punctafile = fullfile(params.punctaSubvolumeDir,sprintf('%s_punctavoxels.mat',params.FILE_BASENAME));
+        load(punctafile);
+        if length(puncta_set_cell)<params.NUM_ROUNDS
+            fprintf('Reprocessing the ROI_collect because %i/%i rounds processed\n',...
+            length(puncta_set_cell),params.NUM_ROUNDS);
+        else
+            fprintf('already processed puncta extraction\n');
+            fprintf('[DONE]\n');
+            return
+        end
     end
 
+    %IN BRANCH: 
     %Because we need to reprocess some fields of view, try using this
     puncta_img_mask_file = fullfile(params.punctaSubvolumeDir,sprintf('%s_allsummedSummedNorm_puncta.%s',params.FILE_BASENAME,params.IMAGE_EXT));
     if ~exist(puncta_img_mask_file,'file')
