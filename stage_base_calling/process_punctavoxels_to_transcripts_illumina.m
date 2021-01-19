70d7ecd1956ccaddc1edefa6b726aa3fa7a71a64
Daniel Goodwin <dgoodwin208@gmail.com>
2021-01-18 21:51:11 -0500

    minor change to account for image_dimensions bug

--- stage_base_calling/process_punctavoxels_to_transcripts_illumina.m
+++ stage_base_calling/process_punctavoxels_to_transcripts_illumina.m
@@ -10,7 +10,7 @@ filename_punctaVol = fullfile(params.punctaSubvolumeDir,sprintf('%s_allsummedSum
 %image dimensions is a utility to get the size of the image without loading
 %just be careful with the XY parameter switch
 IMG_SIZE = image_dimensions(filename_punctaVol);
-if params.IMAGE_EXT=='tif'    
+if strcmp(params.IMAGE_EXT,'tif')
     IMG_SIZE = IMG_SIZE([2,1,3]);
 end
 clear filename_punctaVol
