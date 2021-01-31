769bffd3f351422218a3e85b55507fac3ad2892c
Daniel Goodwin <dgoodwin208@gmail.com>
2021-01-31 14:42:53 -0500

    repo cleanup

--- examplePipelineRunCommand.sh
+++
@@ -1,5 +0,0 @@
-./runPipeline.sh  -N 8 -b splintr3F2 -B 1 #-s "color-correction" #R /home/dgoodwin/ExSeqRepo/registration/
-#./runPipeline.sh  -N 8 -b splintr3F2 -c "'ch00','ch01','ch02','ch03'" -B 1 #-s "color-correction" #R /home/dgoodwin/ExSeqRepo/registration/
-
-#./runPipeline.sh  -N 20 -b exseqautoframe7 -c "'ch00','ch01SHIFT','ch02SHIFT','ch03SHIFT'" -R /home/dgoodwin/ExSeqRepo/registration/ -B 5 -s "color-correction,normalization,puncta-extraction"
-#./runPipeline.sh  -N 20 -b exseqautoframe7 -c "'ch00','ch01SHIFT','ch02SHIFT','ch03SHIFT'" -R /home/dgoodwin/ExSeqRepo/registration/ -B 5 -s "color-correction,normalization,puncta-extraction"
